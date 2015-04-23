82e9ce34b8aa67beba81e8856f52503889dbb13d
junaidAnwar <junaid2905@gmail.com>
2015-04-23 16:57:10 +0530

    Code refactoring with Deepankit,also added ParkingAttendant story part 2

--- com.exercise.carparking/src/ParkingAttendant.java
+++ com.exercise.carparking/src/ParkingAttendant.java
@@ -5,7 +5,10 @@ public class ParkingAttendant {
     private List<ParkingLot> parkingLotList;
 
 
-    public ParkingAttendant(List<ParkingLot> parkingLotList) {
+    public ParkingAttendant(List<ParkingLot> parkingLotList) throws Exception {
+        if(parkingLotList == null || parkingLotList.isEmpty()) {
+            throw new Exception("Cannot Create Parking Attendant as Parking lot list not Present");
+        }
         this.parkingLotList = parkingLotList;
     }
 
@@ -13,13 +16,14 @@ public class ParkingAttendant {
         Ticket ticket;
         ParkingLot parkingLot = getFreeParkingLot();
         int ticketNumber = parkingLot.parkCar(car);
-        ticket = new Ticket(parkingLotList.indexOf(parkingLot), ticketNumber);
+        int parkingLotNumber = parkingLotList.indexOf(parkingLot);
+        ticket = new Ticket(parkingLotNumber, ticketNumber);
         return ticket;
     }
 
     private ParkingLot getFreeParkingLot() throws Exception {
         for (ParkingLot parkingLot : parkingLotList) {
-            if (!parkingLot.isParkingFull()) {
+            if (parkingLot != null && !parkingLot.isParkingFull()) {
                 return parkingLot;
             }
         }
@@ -27,7 +31,14 @@ public class ParkingAttendant {
     }
 
 
-    public Car retrieveParkedCarForTicket(Ticket ticket) {
-        return null;
+    public Car retrieveParkedCarForTicket(Ticket ticket) throws Exception {
+        if(ticket ==  null) {
+            throw new Exception("Invalid Ticket");
+        }
+        ParkingLot parkingLot = parkingLotList.get(ticket.getParkingLotNumber());
+        if(parkingLot == null) {
+            throw  new Exception("Parking lot does not exist");
+        }
+        return parkingLot.retrieveParkedCarForTicket(ticket.getTicketNumber());
     }
 }
--- com.exercise.carparking/testSrc/ParkingAttendantTest.java
+++ com.exercise.carparking/testSrc/ParkingAttendantTest.java
@@ -21,6 +21,15 @@ public class ParkingAttendantTest {
     }
 
     @Test
+    public void shouldNotCreateParkingAttendantForEmptyParkingLotList() throws Exception {
+        try {
+            new ParkingAttendant(null);
+        } catch (Exception e) {
+            assertEquals("Cannot Create Parking Attendant as Parking lot list not Present", e.getMessage());
+        }
+    }
+
+    @Test
       public void shouldParkCarAtFirstParkingLot() throws Exception {
         Car car = new Car();
         Ticket ticket = parkingAttendant.parkCar(car);
@@ -42,6 +51,33 @@ public class ParkingAttendantTest {
         Ticket ticket = parkingAttendant.parkCar(car);
         Car retrievedCar = parkingAttendant.retrieveParkedCarForTicket(ticket);
         assertSame(car, retrievedCar);
+    }
+
+    @Test
+    public void shouldNotRetrieveCarFromNonExistingParkingLot() throws Exception {
+        try {
+            Car car = new Car();
+            parkingAttendant.parkCar(car);
+            parkingAttendant.parkCar(car);
+            Ticket ticket = parkingAttendant.parkCar(car);
+            parkingLotList.set(2, null);
+            parkingAttendant.retrieveParkedCarForTicket(ticket);
+        } catch (Exception e) {
+            assertEquals("Parking lot does not exist", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldNotParkCarIfParkingLotIsFull() throws Exception {
+        try {
+            Car car = new Car();
+            parkingAttendant.parkCar(car);
+            parkingAttendant.parkCar(car);
+            parkingAttendant.parkCar(car);
+            parkingAttendant.parkCar(car);
+        } catch (Exception e) {
+            assertEquals("All parking lots are full.", e.getMessage());
+        }
     }
 
 }
