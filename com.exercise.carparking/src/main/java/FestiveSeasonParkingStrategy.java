baebb2ec52aa79c44ddb78724c4680e5c6944d57
junaidAnwar <junaid2905@gmail.com>
2015-04-24 10:16:14 +0530

    Coding with lakshaya, Srikant, Naveen | added FestveSeason Strategy

---
+++ com.exercise.carparking/src/main/java/FestiveSeasonParkingStrategy.java
@@ -0,0 +1,7 @@
+import java.util.List;
+public class FestiveSeasonParkingStrategy implements ParkingStrategy {
+    @Override
+    public ParkingLot getFreeParkingLot(List<ParkingLot> parkingLotList) throws Exception {
+        return null;
+    }
+}
