779d9d3f8a1151e2202eec6a42e4b4ba9749c557
junaidAnwar <junaid2905@gmail.com>
2015-04-24 12:20:23 +0530

    Coding with lakshaya, Srikant, Naveen | added Festive Story

--- com.exercise.carparking/src/test/java/FestiveSeasonParkingStrategyTest.java
+++ com.exercise.carparking/src/test/java/FestiveSeasonParkingStrategyTest.java
@@ -1,8 +1,28 @@
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
-public class FestiveSeasonParkingStrategyTest extends TestCase {
+import java.util.ArrayList;
 
-    public void testGetFreeParkingLot() throws Exception {
+import static junit.framework.TestCase.assertSame;
 
+public class FestiveSeasonParkingStrategyTest {
+    ParkingStrategy festiveSeasonParkingStrategy;
+    ArrayList<ParkingLot> parkingLotsList;
+    ParkingLot maxSizeParkingLot;
+    @Before
+    public void setUp() throws Exception {
+        festiveSeasonParkingStrategy = new FestiveSeasonParkingStrategy();
+        parkingLotsList = new ArrayList<ParkingLot>();
+        parkingLotsList.add(new ParkingLot(1));
+        parkingLotsList.add(new ParkingLot(2));
+        maxSizeParkingLot = new ParkingLot(3);
+        parkingLotsList.add(maxSizeParkingLot);
+    }
+
+    @Test
+    public void shouldReturnParkingLotWithMaximumSize() throws Exception {
+        ParkingLot parkingLot = festiveSeasonParkingStrategy.getFreeParkingLot(parkingLotsList);
+        assertSame(maxSizeParkingLot, parkingLot);
     }
 }
--- com.exercise.carparking/src/test/java/NormalSeasonParkingStrategyTest.java
+++ com.exercise.carparking/src/test/java/NormalSeasonParkingStrategyTest.java
@@ -1,24 +1,35 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class NormalSeasonParkingStrategyTest {
     NormalSeasonParkingStrategy normalSeasonParkingStrategy;
-
+    ArrayList<ParkingLot> parkingLotsList;
 @Before
-public void setUp(){
+public void setUp() throws Exception {
     normalSeasonParkingStrategy = new NormalSeasonParkingStrategy();
+    parkingLotsList = new ArrayList<ParkingLot>();
+    parkingLotsList.add(new ParkingLot(1));
+    parkingLotsList.add(new ParkingLot(1));
+    parkingLotsList.add(new ParkingLot(1));
 }
 
     @Test
     public void shouldThrowExceptionWhenNoParkingLotListIsPassed() throws Exception {
-
         try {
             normalSeasonParkingStrategy.getFreeParkingLot(null);
         } catch (Exception e) {
            assertEquals("Invalid parking lot list.", e.getMessage());
         }
 
+    }
+
+    @Test
+    public void shouldReturnParkingLot() throws Exception {
+        ParkingLot parkingLot = normalSeasonParkingStrategy.getFreeParkingLot(parkingLotsList);
+        assertNotNull(parkingLot);
     }
 }
